e41258f7b89dcd781d89dfb31fd2785cced0c2b4
nikkiarae <89205483+nikkiarae@users.noreply.github.com>
2021-08-20 09:27:43 +0100

    Delete LearningSwift directory

--- LearningSwift/Playgrounds/Classes_and_Inheritance/Classes_and_Inheritance/main.swift
+++
@@ -1,8 +0,0 @@
-
-
-var player = Player(name: "Nikki", height: 1.92, weight: 67, speed: 67)
-
-var tennisPlayer = TennisPlayer(name: "Kyle", height: 1.65, weight: 63, speed: 98, maxServeSpeed: 121, racket: "Wilson")
-
-player.performSkill()
-tennisPlayer.performSkill()
